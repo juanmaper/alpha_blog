942be761bf26d595ee5dbe84475e0bf1d4929cbb
Juanma <juanmaper30@gmail.com>
2022-04-15 12:49:44 +0200

    Add migration file for admin attribute in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_07_153739) do
+ActiveRecord::Schema.define(version: 2022_04_15_104431) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2022_04_07_153739) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
